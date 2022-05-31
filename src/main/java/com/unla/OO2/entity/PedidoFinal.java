ed63837c50d047fdc2ac2db741de2f3e8f25ed2c
mariaSomephikhay <somephikhay.maria@gmail.com>
2022-05-31 11:26:28 -0300

    actualizacion de las relaciones de pedido final

--- src/main/java/com/unla/OO2/entity/PedidoFinal.java
+++ src/main/java/com/unla/OO2/entity/PedidoFinal.java
@@ -1,16 +1,14 @@
 package com.unla.OO2.entity;
 
-import java.util.Set;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
-import javax.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -22,14 +20,10 @@ public class PedidoFinal {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int idPedidoFinal;
-	@Size(min=1)
-	@ManyToMany(fetch = FetchType.LAZY)	
-	@JoinTable(name = "pedidoFinal_notaPedidos_espacios", joinColumns=@JoinColumn(name="notaPedido_id"),
-	inverseJoinColumns=@JoinColumn(name="pedidoFinal_id"))
-	private Set<NotaPedido> notaPedidos;
-	@Size(min=1)
-	@ManyToMany(fetch = FetchType.LAZY)	
-	@JoinTable(name = "pedidoFinal_notaPedidos_espacios", joinColumns=@JoinColumn(name="espacio_id"),
-	inverseJoinColumns=@JoinColumn(name="pedidoFinal_id"))
-	private Set<Espacio> espacios;
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="espacio_id", nullable=true)
+	private Espacio espacio;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="notaPedido_id", nullable=true)
+	private NotaPedido notaPedido;
 }
